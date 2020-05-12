d70ce84587afff0f999901b798d3e793f2b0c8d9
Jesusisimo <alarconcito_jr@hotmail.com>
2020-05-12 14:13:08 -0400

    agregamos los metodos de lectura asincrona y sincrona

--- prueba.js
+++ prueba.js
@@ -1,10 +1,24 @@
-const fs =  require('fs');
+const fs = require('fs');
 class prueba {
-    ejemploLectura(){
-        fs.readFile('','utf-8',(err, data)=>{
+    ejemploLectura() {
+        console.log('iniciando la lectura')
+        const ruta_archivo = 'archivo.txt';
+        fs.readFile(ruta_archivo,(err, data) => {
+            if (err) {
+                console.log(err)
+            }
             console.log(data);
         })
+        console.log('finalizando la lectura')
+    }
+
+    ejemploLecturaSync() {
+        console.log('iniciando la lectura sincrona')
+        const ruta_archivo = 'archivo.txt'
+        const archivo = fs.readFileSync(ruta_archivo)
+        console.log(archivo)
+        console.log('finalizando la lectura sincrona')
     }
 }
 const objeto = new prueba();
-objeto.ejemploLectura();
+objeto.ejemploLecturaSync();
