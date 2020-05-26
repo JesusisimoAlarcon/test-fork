107d9bdd1de222b9a4e6e0c2144fb96e085d2ae9
Chucho <alarconcito_jr@hotmail.com>
2020-05-25 23:02:36 -0400

    test fork

--- test.js
+++ test.js
@@ -5,15 +5,17 @@ class test {
     ejemploStream() {
         const archivo = fs.createReadStream('./archivo.txt');
         console.log(archivo)
-        
+
         archivo.on('data', (chunk) => {
             console.log(chunk)
             //console.log(chunk.toString())
         });
-        
+
     }
     ejemploLeerArchivo() {
-
+        const ruta = './archivo.txt';
+        const lectura = fs.readFile(ruta);
+        console.log(lectura)
     }
 }
 
